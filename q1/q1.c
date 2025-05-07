0f1aaa82b4abc8086d0df0f36764a0448056dfc1
Felipe Chaves <felipechavespsp@gmail.com>
2025-05-07 13:51:35 -0300

    modularização da função

--- q1/q1.c
+++ q1/q1.c
@@ -1,17 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include "functionq1.h"
 
-bool ehPrimo(int n) {
-    if (n <= 1) {
-        return false;
-    }
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            return false;
-        }
-    }
-    return true;
-}
 
 int main() {
     int a, b, inicio, fim;
