be3e44aae67ba8a2172085ce8d73b937fd725269
Felipe Chaves <felipechavespsp@gmail.com>
2025-05-06 21:16:48 -0300

    resolução da questão 1

--- q1/q1.c
+++ q1/q1.c
@@ -1,5 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdbool.h>
 
-int main(){
-    printf("Hello World");
+bool ehPrimo(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int a, b, inicio, fim;
+
+    printf("Digite dois numeros: ");
+    scanf("%d %d", &a, &b);
+
+    inicio = (a < b) ? a : b;
+    fim = (a < b) ? b : a;
+
+    printf("Numeros primos entre %d e %d:\n", inicio, fim);
+    for (int num = inicio; num <= fim; num++) {
+        if (ehPrimo(num)) {
+            printf("%d ", num);
+        }
+    }
+    printf("\n");
+
+    return 0;
 }
